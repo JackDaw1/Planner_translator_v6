d68bb8540f67407c11134f232b69d733dc1dc9b3
JackDaw1 <iaroshenko.ga@gmail.com>
2023-08-16 17:28:38 +0700

    realised userdefaults for Profile Module

--- PlannerTranslator_v4/Entity/ProfileAPI.swift
+++ PlannerTranslator_v4/Entity/ProfileAPI.swift
@@ -1,10 +1,10 @@
-import Foundation
-
-class ProfileAPI {
-    
-    private init() {}
-    public static let shared = ProfileAPI()
-    
-    public private(set) var profile: ProfileItem =
-        ProfileItem(name: "full name", contact1: "contact1", contact2: "contact2")
-}
+//import Foundation
+//
+//class ProfileAPI {
+//    
+//    private init() {}
+//    public static let shared = ProfileAPI()
+//    
+//    public private(set) var profile: ProfileItem =
+//        ProfileItem(name: "full name", contact1: "contact1", contact2: "contact2")
+//}
--- PlannerTranslator_v4/ProfileDetailRouter/ProfileDetailViewController.swift
+++ PlannerTranslator_v4/ProfileDetailRouter/ProfileDetailViewController.swift
@@ -2,11 +2,15 @@ import UIKit
 
 class ProfileDetailViewController: UIViewController {
     
+//    let userDef = UserDefaults.standard
+    
+    let userDefaults = UserDefaults.standard
+    
     var nameTextField: UITextField = UITextField()
     var contact1TextField: UITextField = UITextField()
     var contact2TextField: UITextField = UITextField()
     
-    var editButton: UIButton = UIButton()
+    var saveButton: UIButton = UIButton()
     let shareButton = UIButton()
     
 //    var presenter: ProfileDetailPresenterProtocol?
@@ -33,6 +37,7 @@ class ProfileDetailViewController: UIViewController {
             nameTextField,
             contact1TextField,
             contact2TextField,
+            saveButton,
             shareButton
             
         ].forEach { customView in
@@ -59,9 +64,15 @@ class ProfileDetailViewController: UIViewController {
             contact2TextField.heightAnchor.constraint(equalToConstant: 60),
             contact2TextField.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor),
             
-            shareButton.leadingAnchor.constraint(equalTo: contact2TextField.leadingAnchor),
-            shareButton.trailingAnchor.constraint(equalTo: contact2TextField.trailingAnchor),
-            shareButton.topAnchor.constraint(equalTo: contact2TextField.bottomAnchor),
+            saveButton.leadingAnchor.constraint(equalTo: contact2TextField.leadingAnchor),
+            saveButton.trailingAnchor.constraint(equalTo: contact2TextField.trailingAnchor),
+            saveButton.topAnchor.constraint(equalTo: contact2TextField.bottomAnchor),
+            saveButton.heightAnchor.constraint(equalToConstant: 60),
+            saveButton.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor),
+            
+            shareButton.leadingAnchor.constraint(equalTo: saveButton.leadingAnchor),
+            shareButton.trailingAnchor.constraint(equalTo: saveButton.trailingAnchor),
+            shareButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor),
             shareButton.heightAnchor.constraint(equalToConstant: 60),
             shareButton.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor),
             
@@ -73,14 +84,39 @@ class ProfileDetailViewController: UIViewController {
         setupConstraints()
         baseConfigure()
         
+        
+        
 //        presenter?.viewDidLoad()
 
+        nameTextField.text = userDefaults.string(forKey: "name")
+        contact1TextField.text = userDefaults.string(forKey: "contact1")
+        contact2TextField.text = userDefaults.string(forKey: "contact2")
+
+        saveButton.setTitle("Сохранить", for: .normal)
+        view.addSubview(saveButton)
+        saveButton.addTarget(self, action: #selector(didTapSaveButton), for: UIControl.Event.touchUpInside)
+        saveButton.setTitleColor(.black, for: .normal)
+        saveButton.backgroundColor = UIColor.lightGray
+        
+//        saveButton.addTarget(self, action: #selector(save), for: UIControl.Event.touchUpInside)
+        
         shareButton.setTitle("Поделиться", for: .normal)
         view.addSubview(shareButton)
         //shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
         shareButton.addTarget(self, action: #selector(didTapShareButton), for: .touchUpInside)
         shareButton.setTitleColor(.black, for: .normal)
         shareButton.backgroundColor = UIColor.lightGray
+        
+        self.prepopulateData()
+
+    }
+    
+    func prepopulateData()
+    {
+      let userDefaults = UserDefaults.standard
+        nameTextField.text =  userDefaults.value(forKey: "name") as! String?
+        contact1TextField.text = userDefaults.value(forKey: "contact1") as! String?
+        contact2TextField.text = userDefaults.value(forKey: "contact2") as! String?
 
     }
     
@@ -93,6 +129,16 @@ class ProfileDetailViewController: UIViewController {
         
         self.present(activityVC, animated: true, completion: nil)
     }
+    
+    @IBAction func didTapSaveButton(_ sender: Any) {
+          userDefaults.setValue(nameTextField.text, forKey: "name")
+          userDefaults.setValue(contact1TextField.text, forKey: "contact1")
+          userDefaults.setValue(contact2TextField.text, forKey: "contact2")
+             
+        let alert = UIAlertController(title: "Alert", message: "Data Saved Successfully", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+              self.present(alert, animated: true, completion: nil)
+       }
 }
 
 
